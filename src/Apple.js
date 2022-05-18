debfc1496b9a3c4b0def5c9184a7d758be682c5f
Kai Zhang <kaizen.agility@gmail.com>
2022-05-18 08:58:03 -0400

    apple transparent

--- src/Apple.js
+++ src/Apple.js
@@ -2,7 +2,7 @@ class Apple {
   constructor(el) {
     this.node = document.createElement('img');
     this.node.setAttribute('id', 'apple');
-    this.node.setAttribute('src', 'src/assets/apple.jpg');
+    this.node.setAttribute('src', 'src/assets/apple.png');
 
     el.appendChild(this.node);
 
